4bd306fbdfc6ebe4e018f0b16aad480f18544792
Houtaro <bonayon.karlo@gmail.com>
2017-09-16 02:43:58 -0700

    Included the database

--- db/grading.sql
+++ db/grading.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2017 at 06:47 PM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: Sep 16, 2017 at 11:42 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `grading`
@@ -26,20 +26,20 @@ SET time_zone = "+00:00";
 -- Table structure for table `tblclass`
 --
 
-CREATE TABLE IF NOT EXISTS `tblclass` (
-`id` int(11) NOT NULL,
+CREATE TABLE `tblclass` (
+  `id` int(11) NOT NULL,
   `classname` varchar(20) NOT NULL,
   `schoolyearid` int(11) NOT NULL,
   `yearlevelid` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblclass`
 --
 
 INSERT INTO `tblclass` (`id`, `classname`, `schoolyearid`, `yearlevelid`) VALUES
-(3, 'class 1', 24, 4),
-(5, 'sa', 24, 4);
+(3, 'BT701E', 24, 4),
+(5, 'BT704P', 24, 4);
 
 -- --------------------------------------------------------
 
@@ -47,19 +47,22 @@ INSERT INTO `tblclass` (`id`, `classname`, `schoolyearid`, `yearlevelid`) VALUES
 -- Table structure for table `tblschoolyear`
 --
 
-CREATE TABLE IF NOT EXISTS `tblschoolyear` (
-`id` int(11) NOT NULL,
-  `schoolyear` varchar(20) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+CREATE TABLE `tblschoolyear` (
+  `id` int(11) NOT NULL,
+  `schoolyear` varchar(20) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblschoolyear`
 --
 
-INSERT INTO `tblschoolyear` (`id`, `schoolyear`) VALUES
-(24, '2016-2017'),
-(4, '2017-2018'),
-(25, '2018-2019');
+INSERT INTO `tblschoolyear` (`id`, `schoolyear`, `status`) VALUES
+(4, '2017-2018', 0),
+(24, '2016-2017', 1),
+(25, '2018-2019', 1),
+(26, '2020-2021', 1),
+(27, '2022-2023', 1);
 
 -- --------------------------------------------------------
 
@@ -67,19 +70,20 @@ INSERT INTO `tblschoolyear` (`id`, `schoolyear`) VALUES
 -- Table structure for table `tblstudentclass`
 --
 
-CREATE TABLE IF NOT EXISTS `tblstudentclass` (
-`id` int(11) NOT NULL,
+CREATE TABLE `tblstudentclass` (
+  `id` int(11) NOT NULL,
   `classid` int(11) NOT NULL,
   `studentid` int(11) NOT NULL,
   `subjectid` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblstudentclass`
 --
 
 INSERT INTO `tblstudentclass` (`id`, `classid`, `studentid`, `subjectid`) VALUES
-(3, 3, 1, 1);
+(3, 3, 1, 1),
+(4, 5, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -87,8 +91,8 @@ INSERT INTO `tblstudentclass` (`id`, `classid`, `studentid`, `subjectid`) VALUES
 -- Table structure for table `tblstudentgrade`
 --
 
-CREATE TABLE IF NOT EXISTS `tblstudentgrade` (
-`id` int(11) NOT NULL,
+CREATE TABLE `tblstudentgrade` (
+  `id` int(11) NOT NULL,
   `studentid` int(11) NOT NULL,
   `schoolyearid` int(11) NOT NULL,
   `subjectid` int(11) NOT NULL,
@@ -100,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `tblstudentgrade` (
   `4thgrading` int(11) NOT NULL,
   `gradeaverage` int(11) NOT NULL,
   `remarks` varchar(20) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblstudentgrade`
@@ -116,12 +120,12 @@ INSERT INTO `tblstudentgrade` (`id`, `studentid`, `schoolyearid`, `subjectid`, `
 -- Table structure for table `tblsubjects`
 --
 
-CREATE TABLE IF NOT EXISTS `tblsubjects` (
-`id` int(11) NOT NULL,
+CREATE TABLE `tblsubjects` (
+  `id` int(11) NOT NULL,
   `subjectname` varchar(50) NOT NULL,
   `description` varchar(50) NOT NULL,
   `yearlevelid` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblsubjects`
@@ -137,19 +141,19 @@ INSERT INTO `tblsubjects` (`id`, `subjectname`, `description`, `yearlevelid`) VA
 -- Table structure for table `tblteacheradvisory`
 --
 
-CREATE TABLE IF NOT EXISTS `tblteacheradvisory` (
-`id` int(11) NOT NULL,
+CREATE TABLE `tblteacheradvisory` (
+  `id` int(11) NOT NULL,
   `teacherid` int(11) NOT NULL,
   `classid` int(11) NOT NULL,
   `subjectid` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblteacheradvisory`
 --
 
 INSERT INTO `tblteacheradvisory` (`id`, `teacherid`, `classid`, `subjectid`) VALUES
-(2, 1, 3, 1);
+(7, 4, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -157,11 +161,11 @@ INSERT INTO `tblteacheradvisory` (`id`, `teacherid`, `classid`, `subjectid`) VAL
 -- Table structure for table `tblyearlevel`
 --
 
-CREATE TABLE IF NOT EXISTS `tblyearlevel` (
-`id` int(11) NOT NULL,
+CREATE TABLE `tblyearlevel` (
+  `id` int(11) NOT NULL,
   `yearlevel` varchar(20) NOT NULL,
   `description` varchar(50) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblyearlevel`
@@ -177,25 +181,30 @@ INSERT INTO `tblyearlevel` (`id`, `yearlevel`, `description`) VALUES
 -- Table structure for table `usertbl`
 --
 
-CREATE TABLE IF NOT EXISTS `usertbl` (
-`id` int(11) NOT NULL,
+CREATE TABLE `usertbl` (
+  `id` int(11) NOT NULL,
   `username` varchar(140) NOT NULL,
   `password` varchar(100) NOT NULL,
   `fname` varchar(140) NOT NULL,
   `mname` varchar(140) NOT NULL,
   `lname` varchar(140) NOT NULL,
   `contact` varchar(140) NOT NULL,
-  `usertype` varchar(50) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `usertype` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `usertbl`
 --
 
-INSERT INTO `usertbl` (`id`, `username`, `password`, `fname`, `mname`, `lname`, `contact`, `usertype`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin', 'admin', '', 'admin'),
-(3, 'student', 'student123', 'student', 'student', 'student', '00988', 'student'),
-(4, 'teacher', 'teacher123', 'teacher', 'teacher', 'teacher', '2424', 'teacher');
+INSERT INTO `usertbl` (`id`, `username`, `password`, `fname`, `mname`, `lname`, `contact`, `usertype`, `status`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin', 'admin', '09173302328', 'admin', 0),
+(3, 'student', 'student123', 'student', 'student', 'student', '00988', 'student', 1),
+(4, 'teachers', 'teacher123', 'teacher', 'teacher', 'teacher', '2424', 'teacher', 0),
+(5, 'asdsad', 'student123', 'dsadas', 'dsadas', 'dsadas', 'dsadas', 'student', 0),
+(6, 'dasdas', 'teacher123', 'dsadas', 'dsadas', 'dsadsa', 'dasdsa', 'teacher', 0),
+(7, 'po', 'teacher123', 'po', 'po', 'po', 'po', 'teacher', 0),
+(8, 'Kaloy', 'admin123', 'Karlo', 'Juanitas', 'Bonayon', '09266493010', 'admin', 0);
 
 --
 -- Indexes for dumped tables
@@ -205,49 +214,54 @@ INSERT INTO `usertbl` (`id`, `username`, `password`, `fname`, `mname`, `lname`, 
 -- Indexes for table `tblclass`
 --
 ALTER TABLE `tblclass`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tblschoolyear`
 --
 ALTER TABLE `tblschoolyear`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `schoolyear` (`schoolyear`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `schoolyear` (`schoolyear`);
 
 --
 -- Indexes for table `tblstudentclass`
 --
 ALTER TABLE `tblstudentclass`
- ADD PRIMARY KEY (`id`), ADD KEY `classid` (`classid`), ADD KEY `studentid` (`studentid`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `classid` (`classid`),
+  ADD KEY `studentid` (`studentid`);
 
 --
 -- Indexes for table `tblstudentgrade`
 --
 ALTER TABLE `tblstudentgrade`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tblsubjects`
 --
 ALTER TABLE `tblsubjects`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tblteacheradvisory`
 --
 ALTER TABLE `tblteacheradvisory`
- ADD PRIMARY KEY (`id`), ADD KEY `teacherid` (`teacherid`,`classid`), ADD KEY `classid` (`classid`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `teacherid` (`teacherid`,`classid`),
+  ADD KEY `classid` (`classid`);
 
 --
 -- Indexes for table `tblyearlevel`
 --
 ALTER TABLE `tblyearlevel`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `usertbl`
 --
 ALTER TABLE `usertbl`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -257,42 +271,42 @@ ALTER TABLE `usertbl`
 -- AUTO_INCREMENT for table `tblclass`
 --
 ALTER TABLE `tblclass`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tblschoolyear`
 --
 ALTER TABLE `tblschoolyear`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- AUTO_INCREMENT for table `tblstudentclass`
 --
 ALTER TABLE `tblstudentclass`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tblstudentgrade`
 --
 ALTER TABLE `tblstudentgrade`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tblsubjects`
 --
 ALTER TABLE `tblsubjects`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tblteacheradvisory`
 --
 ALTER TABLE `tblteacheradvisory`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `tblyearlevel`
 --
 ALTER TABLE `tblyearlevel`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `usertbl`
 --
 ALTER TABLE `usertbl`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
